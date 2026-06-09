a19d5abc06d5519652e1dd34962be6152ca2fe67
Kaustubha10 <kaustubh10k@gmail.com>
2026-06-09 18:53:31 +0530

    CodePush

---
+++ src/main/java/multibranch_demo/MultibranchDemoApplication.java
@@ -0,0 +1,13 @@
+package multibranch_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibranchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multibranch_demo/MultibranchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package multibranch_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibranchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
